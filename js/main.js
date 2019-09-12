ee11a184655b327c836145bbe23c049a6d568ed8
Baldemar Sepulveda <balde.sepu@pm.me>
2019-09-12 16:49:36 -0700

    [main.js] Clear and Delete Complete Basic Input Also Complete

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+let expression = "";
+
+const displayExpressionElement = document.getElementById("expression");
+
+const calculatorButtonWrapper = document.getElementById(
+  "calculator-button-wrapper"
+);
+
+calculatorButtonWrapper.querySelectorAll("p").forEach(button => {
+  button.addEventListener("click", getButtonInput);
+});
+
+function getButtonInput(e) {
+  if (this.dataset.type === "clear") {
+    updateExpression("");
+    updateDisplay("");
+  } else if (this.dataset.type === "delete") {
+    let newExpression = expression.slice(0, -1);
+    updateExpression(newExpression);
+    updateDisplay(newExpression);
+  } else {
+    expression += this.textContent;
+    displayExpressionElement.textContent = expression;
+  }
+}
+
+function updateExpression(input) {
+  expression = input;
+}
+
+function updateDisplay(newExpression) {
+  displayExpressionElement.textContent = newExpression;
+}
+
 function add(operand1, operand2) {
   return operand1 + operand2;
 }
