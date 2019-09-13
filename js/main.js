fcc4c58927338161ca31eed0ac8247979877babe
Baldemar Sepulveda <balde.sepu@pm.me>
2019-09-12 20:12:58 -0700

    Basic Evaluation Complete

--- js/main.js
+++ js/main.js
@@ -11,7 +11,9 @@ calculatorButtonWrapper.querySelectorAll("p").forEach(button => {
 });
 
 function getButtonInput(e) {
-  if (this.dataset.type === "clear") {
+  if (this.dataset.type === "evaluate") {
+    evaluateExpression(expression);
+  } else if (this.dataset.type === "clear") {
     updateExpression("");
     updateDisplay("");
   } else if (this.dataset.type === "delete") {
@@ -22,6 +24,96 @@ function getButtonInput(e) {
     expression += this.textContent;
     displayExpressionElement.textContent = expression;
   }
+}
+
+function evaluateExpression(expression) {
+  if (checkMalformedExpression(expression)) {
+    console.log("malformed expression");
+    return;
+  }
+
+  let operatorsStack = [];
+  let operandsStack = [];
+
+  let operands = expression.match(/[0-9]+/g).map(operand => parseInt(operand));
+  let operators = expression.match(/[+-/*]/g);
+
+  operands.forEach(operand => {
+    operandsStack.push(operand);
+    // if there are operators left to look at
+    // do this
+    if (operators.length > 0) {
+      let currOperator = operators.shift();
+      while (!checkPrecedence(currOperator, peekStack(operatorsStack))) {
+        let operand2 = operandsStack.pop();
+        let operand1 = operandsStack.pop();
+        let eval = evalOperands(operand1, operand2, operatorsStack.pop());
+        operandsStack.push(eval);
+      }
+      operatorsStack.push(currOperator);
+    }
+  });
+
+  console.log(`Operators Stack: ${operatorsStack}`);
+  console.log(`Operands Stack: ${operandsStack}`);
+
+  while (operatorsStack.length > 0) {
+    let operand2 = operandsStack.pop();
+    let operand1 = operandsStack.pop();
+    let eval = evalOperands(operand1, operand2, operatorsStack.pop());
+    operandsStack.push(eval);
+  }
+
+  console.log(`Answer: ${operandsStack[0]}`);
+}
+
+function checkMalformedExpression(expression) {
+  return (
+    expression.charAt(0).match(/[+-/*]/) ||
+    expression.charAt(expression.length - 1).match(/[+-/*]/) ||
+    expression.match(/[+-/*]{2,}/g)
+  );
+}
+
+function evalOperands(operand1, operand2, operator) {
+  switch (operator) {
+    case "+":
+      return add(operand1, operand2);
+      break;
+    case "-":
+      return subtract(operand1, operand2);
+      break;
+    case "*":
+      return multiply(operand1, operand2);
+      break;
+    case "/":
+      return divide(operand1, operand2);
+      break;
+    default:
+      break;
+  }
+}
+
+function checkPrecedence(op1, op2) {
+  if (op1 === "+" || op1 === "-") {
+    if (op2 === "*" || op2 === "/") {
+      return false;
+    } else if (op2 === "+" || op2 === "-") {
+      return true;
+    }
+  } else if (op1 === "*" || op1 === "/") {
+    if (op2 === "+" || op2 === "-") {
+      return true;
+    } else if (op2 === "*" || op2 === "/") {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function peekStack(stack) {
+  return stack.length > 0 ? stack[stack.length - 1] : null;
 }
 
 function updateExpression(input) {
